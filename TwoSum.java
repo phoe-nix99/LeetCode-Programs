8a0c722215857f52ecbf2a12bc5aa8c1577aa7fe
rex99 <chatterjeesoham48@gmail.com>
2025-02-15 15:46:40 +0530

    New

---
+++ TwoSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Sum {
+
+    public int[] twoSum(int[] nums, target) {
+
+        nums = [2, 7, 11, 15];
+        int k = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] + nums[k] == target) {
+
+                return i,j;
+            } else {
+                k++;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        twoSum ob = new twoSum(nums);
+
+
+    }
+}
